a58a140afbc142e7ccdb4e4974f55c98eb1c8c85
apatil88 <amrut.patil88@hotmail.com>
2020-04-12 12:07:57 -0400

    Initial commit

---
+++ covid19_tracker_app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+
+import styles from "./App.module.css";
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <Cards />
+      <Chart />
+      <CountryPicker />
+    </div>
+  );
+};
+
+export default App;
