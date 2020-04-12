7c51af2c44f931fd9f81bbbd666b04cf7fa83112
apatil88 <amrut.patil88@hotmail.com>
2020-04-12 19:17:23 -0400

    Adding cards to display data. Added css styles for each card.

--- covid19_tracker_app/src/App.js
+++ covid19_tracker_app/src/App.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
 
+import { fetchData } from "./api/index";
+
 import styles from "./App.module.css";
 
 const App = () => {
+  const [data, setData] = useState({});
+
+  const fetchTrackerData = async () => {
+    const data = await fetchData();
+    setData(data);
+    return data;
+  };
+
+  useEffect(() => {
+    fetchTrackerData();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Cards />
+      <Cards data={data} />
       <Chart />
       <CountryPicker />
     </div>
---
+++ covid19_tracker_app/src/api/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const URL = "https://covid19.mathdro.id/api";
+
+export const fetchData = async () => {
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(URL);
+
+    const modifiedData = {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+    };
+    return modifiedData;
+  } catch (error) {}
+};
